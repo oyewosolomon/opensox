e2340901731718525e242d2d4dbf6c2849b942e5
oyewosolomon <oyewosolomon@gmail.com>
2025-04-10 11:47:21 +0100

    Updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
     images: {
+      unoptimized: true,
         domains: ['readymadeui.com','placehold.co','picsum.photos'], // add any other external domains here
       },
 };
